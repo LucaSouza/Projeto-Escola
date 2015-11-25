da04ff07d7e00157cc380aa15424e3ae5dafc694
LucaSouza <lucasouzapc@yahoo.com.br>
2015-11-25 12:24:34 -0200

    11:24

--- paginas/principal.php
+++ paginas/principal.php
@@ -14,24 +14,24 @@
                 <form action="" method="post" id="form">
                     <input type="text" class="input-form input100" placeholder="Nome Completo">
                     <input type="text" class=" input-form input25" placeholder="99.999.999-9">
-                    <input type="text" class="input-form input25 " placeholder="Orgão Expedidor">
-                    <input type="select" class="input-form input25 " placeholder="999.999.999-99">
-                    <input type="date" class="input25 " placeholder="Nascimento">
-                    <input type="text" placeholder="Naturalidade">
-                    <input type="text" placeholder="Estado">
-                    <input type="text" placeholder="Sexo">
-                    <input type="text" placeholder="Cor">
-                    <input type="text" placeholder="Profissão">
-                    <input type="text" placeholder="Nome da Mãe">
-                    <input type="text" placeholder="Nome do Pai">
-                    <input type="text" placeholder="Rua">
-                    <input type="text" placeholder="Número">
-                    <input type="text" placeholder="Bairro">
-                    <input type="text" placeholder="Complemento">
-                    <input type="text" placeholder="Cidade">
-                    <input type="text" placeholder="UF">
-                    <input type="text" placeholder="Telefone">
-                    <input type="text" placeholder="Telefone">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Orgão Expedidor">
+                    <input type="select" class="input-form input25 marginleft" placeholder="999.999.999-99">
+                    <input type="date" class="input-form input25 marginleft" placeholder="Nascimento">
+                    <input type="text" class=" input-form input25" placeholder="Naturalidade">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Estado">
+                    <input type="text" class="input-form input5 marginleft" placeholder="Sexo">
+                    <input type="text" class="input-form input20 marginleft" placeholder="Cor">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Profissão">
+                    <input type="text" class="input-form input100" placeholder="Nome da Mãe">
+                    <input type="text" class="input-form input100" placeholder="Nome do Pai">
+                    <input type="text" class=" input-form input25" placeholder="Rua">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Número">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Bairro">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Complemento">
+                    <input type="text" class=" input-form input25" placeholder="Cidade">
+                    <input type="text" class="input-form input25 marginleft" placeholder="UF">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Telefone">
+                    <input type="text" class="input-form input25 marginleft" placeholder="Telefone">
                     <button type="button" id="botao-form">Cadastrar</button>
                 </form>
             </div>
