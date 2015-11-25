cc8c719e24a7e1a163ba3137d01add98a4df3c22
LucaSouza <lucasouzapc@yahoo.com.br>
2015-11-25 12:06:55 -0200

    edicao - 11:07

--- js/scriptPrincipal.js
+++ js/scriptPrincipal.js
@@ -10,6 +10,10 @@ function subRelogio(){
     $("#menu").html("<ul><li id='item-voltar' onclick='menuPrincipal()'>voltarr</li><li id='item-1'>6</li><li id='item-2'>7</li><li id='item-3'>8</li><li id='item-4'>9</li><li id='item-5'>10</li></ul>");
 };
 
+function criarFormCadastroPessoal(){
+    $("#main").html();
+};
+
 $( "#button-menu" ).click(function() {
     $("#menu").toggleClass( "mostraMenu" );
     $("#button-menu").toggleClass("moveButtonMenu");
@@ -20,5 +24,5 @@ $("main").click(function(){
         $("#menu").removeClass( "mostraMenu" );
         $("#button-menu").removeClass("moveButtonMenu");
     }
-})
-$( document ).ready(menuPrincipal())
+});
+$( document ).ready(criarFormCadastroPessoal(), menuPrincipal())
--- paginas/principal.php
+++ paginas/principal.php
@@ -8,8 +8,35 @@
 <body>
     <div id="content">
         <?php include "header.php"?>
-        <main><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, aliquid? Porro a, neque reprehenderit. Voluptatum in at repudiandae, dolor! Exercitationem quos dolor cupiditate, dicta quod quasi at aperiam quisquam.</p></main>
-        <footer><p>asdlfknalsçdknflçank</p></footer>
+        <main id="main">
+            <div id="container-formulario">
+                <div id="cabecario-formulario">Cabeçario</div>
+                <form action="" method="post" id="form">
+                    <input type="text" class="input-form input100" placeholder="Nome Completo">
+                    <input type="text" class=" input-form input25" placeholder="99.999.999-9">
+                    <input type="text" class="input-form input25 " placeholder="Orgão Expedidor">
+                    <input type="select" class="input-form input25 " placeholder="999.999.999-99">
+                    <input type="date" class="input25 " placeholder="Nascimento">
+                    <input type="text" placeholder="Naturalidade">
+                    <input type="text" placeholder="Estado">
+                    <input type="text" placeholder="Sexo">
+                    <input type="text" placeholder="Cor">
+                    <input type="text" placeholder="Profissão">
+                    <input type="text" placeholder="Nome da Mãe">
+                    <input type="text" placeholder="Nome do Pai">
+                    <input type="text" placeholder="Rua">
+                    <input type="text" placeholder="Número">
+                    <input type="text" placeholder="Bairro">
+                    <input type="text" placeholder="Complemento">
+                    <input type="text" placeholder="Cidade">
+                    <input type="text" placeholder="UF">
+                    <input type="text" placeholder="Telefone">
+                    <input type="text" placeholder="Telefone">
+                    <button type="button" id="botao-form">Cadastrar</button>
+                </form>
+            </div>
+        </main>
+        <footer id="footer"></footer>
     </div>
 </body>
 <script src="../js/jquery-1.11.1.min.js"></script>
